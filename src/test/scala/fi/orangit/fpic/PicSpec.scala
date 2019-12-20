53ce5dc773a8001d243f1b97ba71d91c417d39e5
Ville Peurala <ville.peurala@orangit.fi>
2019-12-20 13:56:36 +0200

    Rewrite 'shouldBe' as 'should be(.*)' in tests

--- src/test/scala/fi/orangit/fpic/PicSpec.scala
+++ src/test/scala/fi/orangit/fpic/PicSpec.scala
@@ -16,61 +16,61 @@ class PicSpec extends AnyFlatSpecLike with Matchers {
   behavior of "object Pic, method fromString"
 
   it should "reject an empty string" in {
-    Pic.fromString("") shouldBe Left("Invalid PIC: ''. PIC should have 11 characters, but was 0 characters.")
+    Pic.fromString("") should be(Left("Invalid PIC: ''. PIC should have 11 characters, but was 0 characters."))
   }
 
   it should "reject a PIC with non-numeric character in the first 6 places" in {
-    Pic.fromString("2908X7-1639") shouldBe Left("Invalid PIC: '2908X7-1639'. The first six characters have to be numeric, but they were: '2908X7'.")
+    Pic.fromString("2908X7-1639") should be(Left("Invalid PIC: '2908X7-1639'. The first six characters have to be numeric, but they were: '2908X7'."))
   }
 
   it should "reject a PIC where the delimiter char is something other than +, -, or A" in {
-    Pic.fromString("290877X1639") shouldBe Left("Invalid PIC: '290877X1639'. The sign (7th character) must be +, - or A, now it was: 'X'.")
+    Pic.fromString("290877X1639") should be(Left("Invalid PIC: '290877X1639'. The sign (7th character) must be +, - or A, now it was: 'X'."))
   }
 
   it should "reject a PIC where the individual number is not numeric" in {
-    Pic.fromString("290877-16A9") shouldBe Left("Invalid PIC: '290877-16A9'. The individual number (characters 8-10) must be numeric, now it was: '16A'.")
+    Pic.fromString("290877-16A9") should be(Left("Invalid PIC: '290877-16A9'. The individual number (characters 8-10) must be numeric, now it was: '16A'."))
   }
 
   it should "reject a PIC the control character is wrong" in {
-    Pic.fromString("290877-1638") shouldBe Left("Invalid PIC: '290877-1638'. The control character ('8') is wrong: it should be '9'.")
+    Pic.fromString("290877-1638") should be(Left("Invalid PIC: '290877-1638'. The control character ('8') is wrong: it should be '9'."))
   }
 
   it should "accept a valid PIC" in {
     validPics.foreach(s =>
       Pic.fromString(s) match {
         case Left(errorMsg: String) => fail(errorMsg)
-        case Right(pic) => pic.value shouldBe s
+        case Right(pic) => pic.value should be(s)
       })
   }
 
   it should "uppercase all characters to achieve a canonical representation" in {
-    Pic.fromString("010781-190a").toOption.get.value shouldBe "010781-190A"
-    Pic.fromString("170214a6228").toOption.get.value shouldBe "170214A6228"
+    Pic.fromString("010781-190a").toOption.get.value should be("010781-190A")
+    Pic.fromString("170214a6228").toOption.get.value should be("170214A6228")
   }
 
   behavior of "class Pic"
 
   it should "know the supposed gender of the person in question" in {
-    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.gender shouldBe Male
-    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.gender shouldBe Female
-    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.gender shouldBe Female
+    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.gender should be(Male)
+    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.gender should be(Female)
+    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.gender should be(Female)
   }
 
   it should "know the birth year of the person in question" in {
-    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthYear shouldBe 1977
-    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthYear shouldBe 1981
-    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthYear shouldBe 2014
+    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthYear should be(1977)
+    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthYear should be(1981)
+    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthYear should be(2014)
   }
 
   it should "know the birth month of the person in question" in {
-    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthMonth shouldBe 8
-    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthMonth shouldBe 7
-    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthMonth shouldBe 2
+    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthMonth should be(8)
+    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthMonth should be(7)
+    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthMonth should be(2)
   }
 
   it should "know the birth day (of month) of the person in question" in {
-    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthDay shouldBe 29
-    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthDay shouldBe 1
-    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthDay shouldBe 17
+    Pic.fromString(ValidPic1MaleBornIn1900s).toOption.get.birthDay should be(29)
+    Pic.fromString(ValidPic2FemaleBornIn1900s).toOption.get.birthDay should be(1)
+    Pic.fromString(ValidPic3FemaleBornIn2010s).toOption.get.birthDay should be(17)
   }
 }
