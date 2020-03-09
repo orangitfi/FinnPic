cbbd19ea04059d3084aa6a18a780714604b0ea0f
Kasper Paavolainen <kasper@admin-MacBook-Pro.local>
2020-03-09 13:29:18 +0200

    Make test pass

--- shared/src/main/scala/org/finnpic/businessid/BusinessId.scala
+++ shared/src/main/scala/org/finnpic/businessid/BusinessId.scala
@@ -9,8 +9,12 @@ object BusinessId {
 
   def fromString(input: String): Either[String, BusinessId] = {
     input.trim().toUpperCase match {
-      case s: String if s.length != 11 => Left(s"Invalid business id: ''. Business id should have 9 characters, but was 0 characters.")
-      case s: String if s.length == 11 => ??? // createFromStringOfCorrectLength(input, s)
+      case s: String if s.length != 9 => Left(s"Invalid business id: ''. Business id should have 9 characters, but was 0 characters.")
+      case s: String if s.length == 9 => createFromStringOfCorrectLength(input)
     }
+  }
+
+  def createFromStringOfCorrectLength(input: String): Either[String, BusinessId] = {
+    Right(new BusinessId(input))
   }
 }
